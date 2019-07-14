3440c834b5b6ce8dcfa404b04fcd4ab83be93c5c
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-14 13:33:35 +0100

    Code Random Changed To Static Values For Testing

--- src/main/scala/TextGame/Main.scala
+++ src/main/scala/TextGame/Main.scala
@@ -38,36 +38,35 @@ object Main {
 
     Compass.compassMain(xDirection, xExit, xEvent, yDirection, yExit, yEvent)
 
-    if (xDirection == xEvent && yDirection == yEvent) {
-      FindingEvents.findEventText()
-      val xEventNew = 2
-      val yEventNew = 2
-        Combat.combatStart(player)
-    }
-    else if (xDirection == xExit && yDirection == yExit) {
-      FindingEvents.findExitText(player.name,player.playerClass)
-      val inputExit = scala.io.StdIn.readLine()
-      if (inputExit.toLowerCase().contains("no") || inputExit.toLowerCase().contains("n")) {
-        TitleText("gameoverghost")
-        sys.exit()
-      }
-      else {
-        val newPLayer = characterCreation()
-        val xDirection = 0
-        val yDirection = 0
-        val xExit = 3
-        val yExit = 3
-        mainGame(xDirection ,yDirection ,Random.integerValue(10) ,Random.integerValue(10) ,Random.integerValue(10) ,Random.integerValue(10),newPLayer)
-        Compass.compassMain(xDirection,xExit,xEvent,yDirection,yExit,yEvent)
-      }
-    }
-    else if (yDirection == yExit) {
-      FindingEvents.yAlignedWithExitText()
-    }
-    else if (xDirection == xExit) {
-      FindingEvents.xAlignedWithExitText()
-    }
 
+    xDirection match {
+      case x if x == xEvent && yDirection == yEvent
+        => FindingEvents.findEventText()
+        val xEventNew = 2
+        val yEventNew = 2
+        Combat.combatStart(player)
+
+      case x if x == xExit && yDirection ==yExit =>
+        FindingEvents.findExitText(player.name,player.playerClass)
+        val inputExit = scala.io.StdIn.readLine()
+        if (inputExit.toLowerCase().contains("no") || inputExit.toLowerCase().contains("n")) {
+          TitleText("gameoverghost")
+          sys.exit()}
+          else {
+            val newPLayer = characterCreation()
+            val xDirection = 0
+            val yDirection = 0
+            val xExit = 3
+            val yExit = 3
+            mainGame(xDirection, yDirection, Random.integerValue(10), Random.integerValue(10), Random.integerValue(10), Random.integerValue(10), newPLayer)
+            Compass.compassMain(xDirection, xExit, xEvent, yDirection, yExit, yEvent)
+          }
+      case x if yDirection == yExit => FindingEvents.yAlignedWithExitText()
+
+      case x if x ==xExit => FindingEvents.xAlignedWithExitText()
+
+      case _ => println("You Are Still Searching")
+    }
     mainGame(xDirection,yDirection,xExit,yExit,yEvent,xEvent,player)
   }
 }
