ee1bb24570b5e846b317e319792e4ee3a7efdfb4
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-15 01:07:17 +0100

    Currency Exchange System Changed ,Now Working

--- src/main/scala/TextGame/ShopsAndItems/CurrencyLogic.scala
+++ src/main/scala/TextGame/ShopsAndItems/CurrencyLogic.scala
@@ -1,61 +1,63 @@
 package TextGame.ShopsAndItems
 
 
-
 object CurrencyLogic {
 
-  val checkDone = false
-
   def main(args: Array[String]): Unit = {
-    val test :List[Int] = currencyChecker(10,-20,300)
-    println(test)
+    val testingclass = currencyChecker(10,10,11)
+    println(testingclass)
+    println("Done" + testingclass)
   }
 
+  @annotation.tailrec
   def currencyChecker(goldIn: Int, silverIn: Int, bronzeIn: Int,doneChecking :Boolean = false): List[Int] = {
-    Thread.sleep(200)
+    Thread.sleep(100)
     println(s"Gold : $goldIn ,Silver : $silverIn ,Bronze : $bronzeIn")
+
+    var gold: Int = goldIn
+    var silver: Int = silverIn
+    var bronze: Int = bronzeIn
+
     silverIn match {
       case x if x < 0 && goldIn > 0 =>
-        val silverUpdated = silverIn + 10
-        val goldUpdated = goldIn - 1
-        currencyChecker(goldUpdated, silverUpdated, bronzeIn)
-        sys.exit()
-      case x if x > 100 =>
-        val silverUpdated = silverIn - 100
-        val goldUpdated = goldIn + 10
-        currencyChecker(goldUpdated, silverUpdated, bronzeIn)
-        sys.exit()
+        silver = silverIn + 10
+        gold = goldIn - 1
+      //currencyChecker(goldUpdated, silverUpdated, bronzeIn)
+
+      case x if x >= 100 =>
+        silver = silverIn - 100
+        gold = goldIn + 10
+      //currencyChecker(goldUpdated, silverUpdated, bronzeIn)
+
       case x if x >= 10 =>
-        val silverUpdated = silverIn - 10
-        val goldUpdated = goldIn + 1
-        currencyChecker(goldUpdated, silverUpdated, bronzeIn)
-        sys.exit()
+        silver = silverIn - 10
+        gold = goldIn + 1
+      //currencyChecker(goldUpdated, silverUpdated, bronzeIn)
+
       case _ =>
     }
     bronzeIn match {
       case x if x < 0 && silverIn > 0 =>
-        val bronzeUpdated = bronzeIn + 10
-        val silverUpdated = silverIn - 1
-        currencyChecker(goldIn, silverUpdated, bronzeUpdated)
-        sys.exit()
-      case x if x > 100 =>
-        val bronzeUpdated = bronzeIn - 100
-        val silverUpdated = silverIn + 10
-        currencyChecker(goldIn, silverUpdated, bronzeUpdated)
-       sys.exit()
+        bronze = bronzeIn + 10
+        silver = silver - 1
+      currencyChecker(goldIn, silver, bronze)
+
+      case x if x >= 100 =>
+        bronze = bronzeIn - 100
+        silver = silver + 10
+      currencyChecker(goldIn, silver, bronze)
+
       case x if x >= 10 =>
-        val bronzeUpdated = bronzeIn - 10
-        val silverUpdated = silverIn + 1
-        currencyChecker(goldIn, silverUpdated, bronzeUpdated)
-        sys.exit()
-      case _ =>
+        bronze = bronzeIn - 10
+        silver = silverIn + 1
+      currencyChecker(goldIn, silver, bronze)
+
+      case _ if goldIn > 0 && silverIn > 0 & bronzeIn > 0 =>
+        val newCurrency: List[Int] = List(gold, silver, bronze)
+        println(s"Gold   : ${newCurrency(0)} \nSilver : ${newCurrency(1)} \nBronze : ${newCurrency(2)}")
+        println(newCurrency.toString())
+        //currencyChecker(goldIn,silverIn,bronzeIn)
+        newCurrency
     }
-    if (goldIn > 0 && silverIn > 0 & bronzeIn > 0) {
-      val newCurrency: List[Int] = List(goldIn, silverIn, bronzeIn)
-      println(s"Gold   : ${newCurrency(0)} \nSilver : ${newCurrency(1)} \nBronze : ${newCurrency(2)}")
-      println(newCurrency.toString())
-      newCurrency
-    }
-    List(goldIn,silverIn,bronzeIn)
   }
 }
